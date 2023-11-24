1488ac5509015af3bda253933fd45f39421398c3
brunoborges <bborgesfilho33@gmail.com>
2023-11-23 21:51:14 -0300

    first commit

---
+++ src/Router/index.tsx
@@ -0,0 +1,34 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AuthLayout } from '../view/layouts/AuthLayout';
+import { DashBoard } from '../view/pages/DashBoard';
+import { Login } from '../view/pages/Login';
+import { Register } from '../view/pages/Register';
+import { AuthGuard } from './AuthGuard';
+
+export function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AuthGuard isPrivate={false} />}>
+          <Route element={<AuthLayout />}>
+            <Route
+              path='/login'
+              element={<Login />}
+            />
+            <Route
+              path='/register'
+              element={<Register />}
+            />
+          </Route>
+        </Route>
+
+        <Route element={<AuthGuard isPrivate />}>
+          <Route
+            path='/'
+            element={<DashBoard />}
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
